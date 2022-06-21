77f109ae1166ed262173c4c0941e9b5b9352aeb8
sarkav14 <104012381+sarkav14@users.noreply.github.com>
2022-06-22 03:23:01 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python3
 # This Phython program will print Hellow World...
-print("Hello World Demo...22 Jun 2022 \n")
+print("Hello World Demo...22 Jun 2022 git webhook trigger \n")
 
 
 
